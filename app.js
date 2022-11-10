65647d9f00d3a5a80b65e243d9220684e19eb0ec
Omarfaruk05 <mdomarfrauk149518.com>
2022-11-10 20:44:40 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+
+app.use(express.json());
+app.use(cors());
+
+
+
+app.get("/", (req, res)=>{
+    res.send("Wellcome to E-Mart server. All routes are working.")
+});
+
+app.all("*", (req, res)=> {
+    res.send("Opps...No Route Found.")
+});
+
+module.exports = app;
